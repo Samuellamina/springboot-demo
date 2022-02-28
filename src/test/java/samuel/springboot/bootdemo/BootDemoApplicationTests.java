f44305997c17b4d6927c6693cc39d9b1f889eb7d
Lamina Samuel <samuellamina2@gmail.com>
2022-02-28 07:49:15 +0100

    init

---
+++ src/test/java/samuel/springboot/bootdemo/BootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package samuel.springboot.bootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
